336f0d1398f29fc370ff02b256a97125272be919
Torubiri Walter <toruwalt1997@gmail.com>
2024-07-24 15:53:21 +0100

    ES6 Promise - Task 1:Attempt 3

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      data = {status: 200, body: 'Success'};
+      const data = {status: 200, body: 'Success'};
       resolve(data);
     } else {
       reject("The fake API is not working currently");
