0dc13523e07a9c492e1dfaa05cd35594f46dfb12
Torubiri Walter <toruwalt1997@gmail.com>
2024-07-24 22:27:57 +0000

    ES6 Promise - Task 2:Attempt 4

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,17 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  }
-  myPromise.then((result) => {
-    console.log("Got a response from the API:", result);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+	return promise
+	.then({status: 200, body: 'success'})
+	.catch(new Error())
+	.finally(console.log("Got a response from the API"))
 }
