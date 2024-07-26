3605abdc54d2ff7227032de4d1a369dd2219dd1c
Torubiri Walter <toruwalt1997@gmail.com>
2024-07-26 12:45:08 +0100

    ES6 Classes - Task 1:Attempt 2

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,9 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  let rooms = [];
+  const rooms = [];
   rooms.push(new ClassRoom(19));
   rooms.push(new ClassRoom(20));
   rooms.push(new ClassRoom(34));
   return rooms;
 }
-
