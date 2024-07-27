affc30691b7cde0f3f55ee9a633bfda5d315ca47
Torubiri Walter <toruwalt1997@gmail.com>
2024-07-28 00:00:30 +0100

    ES6 Classes - Task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,41 @@
+export default class Airport {
+  constructor(code, name) {
+    if (typeof (code) !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else {
+      this._code = code;
+    }
+
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  toString() {
+    return this._name;
+  }
+}
