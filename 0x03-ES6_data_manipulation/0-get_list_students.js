b897287e8d7bb76354ae255202d627674af5436a
Torubiri Walter <toruwalt1997@gmail.com>
2024-07-30 12:58:49 +0100

    ES6 Data Manipulation - Task 0:Attempt 2

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,8 @@
-export default function getListStudents () {
-    const array = [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 3, firstName: 'Serena', location: 'San Francisco' }
-    ];
-
-    return array;
-};
+export default function getListStudents() {
+  const array = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 3, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return array;
+}
