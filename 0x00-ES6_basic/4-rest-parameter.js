95a98b97e323276d9980c7ccc37f30ca92f6212c
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-07 22:47:18 +0100

    ES6 Basics - Task 4:Attempt 5

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,16 @@
 export default function returnHowManyArguments(...strings) {
-  let count =  0;
-  for (const string of strings) {
+  let count = 0;
+
+  strings.forEach(() => {
+    count += 1;
+  })
+  return count;
+}
+
+/*
+  for (let string of strings) {
     count += 1;
   }
   return count;
 }
+*/
