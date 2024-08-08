22cafde3e7546546f079c93c2ea9bffd8f681276
Torubiri Walter <toruwalt1997@gmail.com>
2024-08-08 23:03:48 +0100

    ES6 Basics - Task 11:Attempt 2

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,10 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const x = [];
-  for (const employee of employees) {
-    x.push(employee);
-  }
-
   return {
-    [departmentName]: x,
+    [departmentName]: employees,
   };
 }
