6d6d5bd0c9b9260892d44c68f15e3a7973376bf7
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-30 22:43:37 +0100

    NodeJS Basics - Task 1:Attempt 2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,6 +6,6 @@ const rl = readline.createInterface({
 });
 
 rl.question('Welcome to Holberton School, what is your name? \n', (name) => {
-  console.log(`Hello, ${name}!`);
+  console.log(`Your name is: ${name}`);
   rl.close();
 });
