edfdf27812309714621b62e2df5e727e31c3540c
Torubiri Walter <toruwalt1997@gmail.com>
2024-10-12 18:04:36 +0100

    NodeJS Basics - Task 4:Attempt 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,14 @@
 const http = require('http');
 const porty = 1245;
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-server.listen(porty, () => {
+app.listen(porty, () => {
   console.log('...');
 });
+
+module.exports = app;
